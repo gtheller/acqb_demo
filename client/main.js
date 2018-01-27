3080d8dbc90835bd8b65b139e8145ff7da929008
gtheller <gtheller@umich.edu>
2018-01-27 01:21:53 -0500

    working manifest

--- client/main.js
+++ client/main.js
@@ -4,8 +4,5 @@ import { render } from 'react-dom';
 import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
-
   render(<App />, document.getElementById('render-target'));
-  document.body.requestFullscreen();
-
 });
